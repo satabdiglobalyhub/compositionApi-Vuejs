b63e2107e96d297b70a7962580b33530ebd83f6b
satabdiglobalyhub <satabdi.gautam@globalyhub.com>
2023-02-13 16:38:20 +0545

    composition api

---
+++ src/router/index.js
@@ -0,0 +1,22 @@
+import { createRouter, createWebHistory } from "vue-router";
+import Home from "../views/Home.vue";
+import Details from "../views/Details.vue";
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: "/",
+      name: "home",
+      component: Home,
+    },
+    {
+      path: "/posts/:id",
+      name: "Details",
+      component: Details,
+      props: true,
+    },
+  ],
+});
+
+export default router;
